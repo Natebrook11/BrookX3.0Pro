93605232fe893dfaa4bed5584267f1b5099f8f31
Nate <106435163+Natebrook11@users.noreply.github.com>
2023-04-07 13:42:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,191 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Navbar';
+
+const Navbar = () => {
+
+  // Desktop Navigation
+  const desktopNavElement = useRef(null);
+  const searchContainerElement = useRef(null);
+  const overlayElement = useRef(null);
+
+  const addSearch = () => {
+    desktopNavElement.current.classList.add("hide");
+    searchContainerElement.current.classList.remove("hide");
+    overlayElement.current.classList.add("show");
+  };
+
+  const addClose = () => {
+    desktopNavElement.current.classList.remove("hide");
+    searchContainerElement.current.classList.add("hide");
+    overlayElement.current.classList.remove("show");
+  };
+
+  const addOverlay = () => {
+    desktopNavElement.current.classList.remove("hide");
+    searchContainerElement.current.classList.add("hide");
+    overlayElement.current.classList.remove("show");
+  };
+
+  // Mobile Navigation
+
+  const navContainerElement = useRef(null);
+  const searchBarElement = useRef(null);
+  const navElement = useRef(null);
+
+  const addMobMenu = () => {
+    navContainerElement.current.classList.toggle("active");
+  };
+
+  const addMobSearch = () => {
+    searchBarElement.current.classList.add("active");
+    navElement.current.classList.add("move-up");
+    desktopNavElement.current.classList.add("move-down");
+  };
+
+  const addMobCancel = () => {
+    searchBarElement.current.classList.remove("active");
+    navElement.current.classList.remove("move-up");
+    desktopNavElement.current.classList.remove("move-down");
+  };
+
+  return (
+    <>
+      <div className="nav-container" ref={navContainerElement}>
+        <nav ref={navElement}>
+          <ul className="mobile-nav">
+            <li>
+              <div className="menu-icon-container" onClick={() => addMobMenu()}>
+                <div className="menu-icon">
+                  <span className="line-1"></span>
+                  <span className="line-2"></span>
+                </div>
+              </div>
+            </li>
+
+            <li>
+              <a href="#" className="link-logo"></a>
+            </li>
+
+            <li>
+              <a href="" className="link-bag"></a>
+            </li>
+          </ul>
+
+          <ul className="desktop-nav" ref={desktopNavElement}>
+            <li>
+              <a href="#" className="link-logo"></a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#main">Apps</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Skills</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Support</a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="link-search"
+                onClick={() => addSearch()}
+              ></a>
+            </li>
+            <li>
+              <a href="#" className="link-bag"></a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="search-container hide" ref={searchContainerElement}>
+          <div className="link-search" onClick={() => addSearch()}></div>
+          <div className="search-bar" ref={searchBarElement}>
+            <form action="">
+              <input type="text" placeholder="Search here ..." />
+            </form>
+          </div>
+          <div className="link-close" onClick={() => addClose()}></div>
+
+          <div className="quick-links">
+            <h2>Quick Links</h2>
+            <ul>
+              <li>
+                <a href="#">Visiting an Apple Store FAQ</a>
+              </li>
+              <li>
+                <a href="#">Shop Apple Store Online</a>
+              </li>
+              <li>
+                <a href="#">Accessories</a>
+              </li>
+              <li>
+                <a href="#">AirPods</a>
+              </li>
+              <li>
+                <a href="#">AirTag</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="mobile-search-container" ref={searchBarElement}>
+          <div className="link-search" onClick={() => addSearch()}></div>
+          <div className="search-bar" ref={searchBarElement}>
+            <form action="">
+              <input
+                type="text"
+                placeholder="Search here ..."
+                onClick={() => addMobSearch()}
+              />
+            </form>
+          </div>
+          <span className="cancel-btn" onClick={() => addMobCancel()}>
+            Cancel
+          </span>
+
+          <div className="quick-links">
+            <h2>Quick Links</h2>
+            <ul>
+              <li>
+                <a href="#">Visiting an Apple Store FAQ</a>
+              </li>
+              <li>
+                <a href="#">Shop Apple Store Online</a>
+              </li>
+              <li>
+                <a href="#">Accessories</a>
+              </li>
+              <li>
+                <a href="#">AirPods</a>
+              </li>
+              <li>
+                <a href="#">AirTag</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div
+        className="overlay"
+        ref={overlayElement}
+        onClick={() => addOverlay()}
+      ></div>
+    <SmoothScrollButton/>
+    </>
+  );
+}; 
+
+export default Navbar;
 
 function App() {
   return (
